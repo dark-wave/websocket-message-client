ef4e60d29986774d66a10451f37cd589d882caa2
Noé Montes <noemontes@MacBook-Pro-de-Noe.local>
2021-02-05 17:34:38 +0100

    Proyecto cliente de mensajeria con Spring Websocket

---
+++ src/test/java/dev/noemontes/websocketclient/WebsocketMessagesClientApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.noemontes.websocketclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketMessagesClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
